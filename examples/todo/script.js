076b7f22ab2936b7e480b95a5b9c7c3a611c2a48
amveex <amveex@gmail.com>
2025-09-20 07:07:59 +0300

    last commit

--- examples/todo/script.js
+++ examples/todo/script.js
@@ -4,12 +4,15 @@ let input = document.getElementById("INPUT");
 let task;
 let text;
 let btn;
+let div;
+let i = 0;
 
 function addTask() {
 
     // creates "div" for the task
     task = document.createElement("div");
     task.setAttribute("class", "task");
+    task.setAttribute("id", `${i}`);
     body.appendChild(task);
 
     // creates "p" for the task
@@ -21,14 +24,20 @@ function addTask() {
     // creates "button" for the task
     btn = document.createElement("button");
     btn.setAttribute("class", "delete");
-    btn.setAttribute("onclick", "delTask()");
+    btn.setAttribute("onclick", `delTask(${i})`);
     btn.textContent = "X";
     task.appendChild(btn);
     
     // clears the input
     input.value = null;
+
+    // increments "id" for new tasks
+    if (true) {
+        i++;
+    }
 }
 
-function delTask() {
-    
+function delTask(id) {
+    div = document.getElementById(id);
+    div.remove();
 }
