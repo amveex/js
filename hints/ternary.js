25fd9ff36a0c2127a302c2439bb25a8048938039
amveex <amveex@gmail.com>
2025-10-20 00:14:14 +0300

    for myself

---
+++ hints/ternary.js
@@ -0,0 +1,19 @@
+// three operands below
+// condition
+// ? trueExpression
+// : falseExpression
+
+let score = 80;
+let scoreRating;
+
+// first example
+
+if(score > 70) {
+    scoreRating = "Excellent";
+} else {
+    scoreRating = "Can do better";
+}
+
+// second example
+
+scoreRating = score > 70 ? "Excellent" : "Can do better";
