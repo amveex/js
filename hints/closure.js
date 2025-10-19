062a580ea9dd4503f1178446f7e54f69361b1c49
amveex <amveex@gmail.com>
2025-10-19 23:00:20 +0300

    for myself

---
+++ hints/closure.js
@@ -0,0 +1,15 @@
+// CLOSURE IS A FUNCTION DEFINED INSIDE OF ANOTHER FUNCTION
+// DEFINED FUNCTION HAS ACCESS TO THE VARIABLES AND SCOPE
+// EVEN IF THE ANOTHER FUNCTION HAS FINISHED EXECUTION
+
+function another() {
+    let msg = "private"
+
+    function defined() {
+        console.log(msg);
+    }
+
+    defined();
+}
+
+another();
