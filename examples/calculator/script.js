6de70d4752d734ebabb69cf4407d42048dc3f431
amveex <amveex@gmail.com>
2026-03-25 04:26:31 +0300

    fixed calculator

--- examples/calculator/script.js
+++ examples/calculator/script.js
@@ -1,18 +1,50 @@
 let field = document.getElementById("FIELD");
+let A = null;
+let numA = null;
+let B = null;
+let numB = null;
+let symbol = null;
 
-function pick(symbol) {
-    let value = symbol;
-    field.value += value;
-}
 
 function reset() {
     field.value = "";
+    A = null;
+    numA = null;
+    B = null;
+    numB = null;
+    symbol = null;
 }
 
+
+function pickNumber(number) {
+    field.value += number;
+}
+
+
+function pickOperator(operator) {
+    A = field.value;
+    symbol = operator;
+    field.value = "";
+}
+
+
 function calculate() {
-    try {
-        field.value = eval(field.value);
-    } catch (err) {
-        field.value = "Error";
+    B = field.value;
+    numA = Number(A);
+    numB = Number(B);
+
+    switch (symbol) {
+        case '+':
+            field.value = numA + numB;
+            break;
+        case '-':
+            field.value = numA - numB;
+            break;
+        case '*':
+            field.value = numA * numB;
+            break;
+        case '/':
+            field.value = numA / numB;
+            break;
     }
 }
